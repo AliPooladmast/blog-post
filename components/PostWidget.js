301cc575313b597b170235f928e39cbcdd4f6e44
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-15 13:31:02 +0200

    add logic for getting widget posts

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -1,6 +1,23 @@
-import React from "react";
+import { getRecentPosts, getSimilarPosts } from "../services";
+import React, { useEffect, useState } from "react";
 
-const PostWidget = () => {
+const PostWidget = ({ slug, categories }) => {
+  const [relatedPost, setRelatedPost] = useState([]);
+
+  useEffect(() => {
+    getWidgetPosts();
+  }, []);
+
+  const getWidgetPosts = async () => {
+    if (slug) {
+      const result = await getSimilarPosts(slug, categories);
+      setRelatedPost(result);
+    } else {
+      const result = await getRecentPosts();
+      setRelatedPost(result);
+    }
+  };
+
   return <div>PostWidget</div>;
 };
 
