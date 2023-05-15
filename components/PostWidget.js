c289b540ba24e02f1cb62b9ac555df6575e89596
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-15 13:33:30 +0200

    add title for post widget

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -18,7 +18,13 @@ const PostWidget = ({ slug, categories }) => {
     }
   };
 
-  return <div>PostWidget</div>;
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+    </div>
+  );
 };
 
 export default PostWidget;
