e428844bc0076cda5c7e87a53f6db3ce19f8514c
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-16 13:18:42 +0200

    create a context for categories

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,27 @@
+import { createContext, useEffect, useState } from "react";
 import Header from "./Header";
+import { getCategories } from "@/services";
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    {children}
-  </>
-);
+export const Context = createContext(null);
+
+const Layout = ({ children }) => {
+  const [categories, setCategories] = useState([]);
+
+  const categoriesRequest = async () => {
+    const result = await getCategories();
+    setCategories(result);
+  };
+
+  useEffect(() => {
+    categoriesRequest();
+  }, []);
+
+  return (
+    <Context.Provider value={categories}>
+      <Header />
+      {children}
+    </Context.Provider>
+  );
+};
 
 export default Layout;
