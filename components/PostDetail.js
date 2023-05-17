f17be7e763a543221ac6e419954f80eebd303cd9
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-17 17:17:43 +0200

    complete the sub section of the post detail

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SubPost from "./SubPost";
 
 const PostDetail = ({ post }) => {
   return (
@@ -9,6 +10,13 @@ const PostDetail = ({ post }) => {
           alt={post.title}
           className="object-top h-full w-full rounded-t-lg"
         />
+      </div>
+      <div className="px-4 lg:px-0">
+        <div className="flex items-center mb-8 w-full">
+          <SubPost post={post} />
+        </div>
+
+        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
       </div>
     </div>
   );
