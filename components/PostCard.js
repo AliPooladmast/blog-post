eaf2d0ffc9b2ee57e207dd626c7ba5ec1351b3c7
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-17 17:13:38 +0200

    specify the sub section of the post to a seperate component

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import moment from "moment/moment";
+import SubPost from "./SubPost";
 
 const PostCard = ({ post }) => {
   return (
@@ -18,37 +18,7 @@ const PostCard = ({ post }) => {
       </h1>
 
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-          <img
-            src={post.author.photo.url}
-            alt={post.author.name}
-            height="30px"
-            width="30px"
-            className="align-middle rounded-full"
-          />
-          <p className="inline align-middle text-gray-700 ml-2 text-lg">
-            {post.author.name}
-          </p>
-        </div>
-
-        <div className="font-medium text-gray-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 inline mr-2 text-pink-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
-
-          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
-        </div>
+        <SubPost post={post} />
       </div>
 
       <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
