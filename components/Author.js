dfb9d49997a7fd6d6d87fee54be241c59f135268
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-18 21:16:56 +0200

    create author section for the post

--- components/Author.js
+++ components/Author.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Author = () => {
-  return <div>Author</div>;
+const Author = ({ author }) => {
+  return (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 -top-24 w-full h-full flex justify-center items-center">
+        <img
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full object-cover"
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
 };
 
 export default Author;
