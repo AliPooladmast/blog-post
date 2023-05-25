aeb02bce170c878b746d53b1744ed8e30f50661a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-25 17:10:16 +0200

    create content and style for comment list

--- components/Comments.js
+++ components/Comments.js
@@ -1,4 +1,5 @@
 import { getComments } from "@/services";
+import moment from "moment";
 import React, { useEffect, useState } from "react";
 
 const Comments = ({ slug }) => {
@@ -13,7 +14,33 @@ const Comments = ({ slug }) => {
     setComments(result);
   };
 
-  return <div>Comments</div>;
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length} Comments
+          </h3>
+
+          {comments.map((comment) => (
+            <div
+              key={comment.createdAt}
+              className="border-b border-gray-100 mb-4 pb-4"
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name} </span> on{" "}
+                {moment(comment.createdAt).format("MMM DD, YYYY")}
+              </p>
+
+              <p className="whitespace-pre-line text-gray-60 w-full">
+                {comment.comment}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Comments;
