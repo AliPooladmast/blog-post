bd43f772af9cbbbb669942deb18e42f90e904cb1
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-25 15:39:03 +0200

    get the comments inside the module

--- components/Comments.js
+++ components/Comments.js
@@ -1,6 +1,18 @@
-import React from "react";
+import { getComments } from "@/services";
+import React, { useEffect, useState } from "react";
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getCommentData();
+  }, []);
+
+  const getCommentData = async () => {
+    const result = await getComments(slug);
+    setComments(result);
+  };
+
   return <div>Comments</div>;
 };
 
