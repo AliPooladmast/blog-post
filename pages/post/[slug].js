6b376432d6764f9dfa80ee0257ffb3150c30ec31
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-05-25 23:38:26 +0200

    change fallback of post page to make more dynamic

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,13 +3,21 @@ import {
   Categories,
   Comments,
   CommentsForm,
+  Loader,
   PostDetail,
   PostWidget,
 } from "@/components";
 import { getPostDetails, getPosts } from "@/services";
+import { useRouter } from "next/router";
 import React from "react";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -44,7 +52,7 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   };
 }
 
